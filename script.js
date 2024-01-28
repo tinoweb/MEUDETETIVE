3ea68fb1add88f62d235eadf1d04966f79f6e4f5
tinoweb <tino477@gmail.com>
2024-01-27 23:56:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    // Exemplo: Exibir um alerta quando o modal abre
+    $('#serviceModal').on('show.bs.modal', function (e) {
+        console.log('O modal está prestes a ser exibido!');
+    });
+    // Aqui você pode adicionar mais interações conforme necessário
+});
+  
